fabbff2d230871260f02c6927e38064a5704504c
Chihhung Jen <quanistcoding@gmail.com>
2023-02-21 16:31:15 +0800

    Seeded roles into database.

---
+++ Rental Store Management/Data/Migrations/20230221082504_SeedRolesIntoDatabase.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Rental_Store_Management.Data.Migrations
+{
+    public partial class SeedRolesIntoDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+           table: "AspNetRoles",
+           columns: new[] { "Id", "Name" },
+           values: new object[,]
+           {
+                { "1","admin" },
+                { "2","employee" },
+                { "3","customer" }
+       });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
