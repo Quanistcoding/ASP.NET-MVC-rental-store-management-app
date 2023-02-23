7d3019f808611189da4d8e6f5ea619f5a688a2da
Chihhung Jen <quanistcoding@gmail.com>
2023-02-23 09:29:24 +0800

    Modified Rental MVC.

--- Rental Store Management/Data/Migrations/20230223012159_CreatedRentaltable.Designer.cs
+++ Rental Store Management/Data/Migrations/20230223012159_CreatedRentaltable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Rental_Store_Management.Data;
 
@@ -11,9 +12,10 @@ using Rental_Store_Management.Data;
 namespace Rental_Store_Management.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230223012159_CreatedRentaltable")]
+    partial class CreatedRentaltable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Rental Store Management/Models/Rental.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Rental_Store_Management.Models
+{
+    public class Rental
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public Customer? Customr { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        [DisplayName("Driver's License No.")]
+        public string  CustomerDriverLicenseNumber { get; set; }
+
+        public Movie? Movie { get; set; }
+
+        [Required]
+        [DisplayName("Movie")]
+        public int MovieId { get; set; }
+
+        [Required]
+        [DisplayName("Date Rented")]
+        public DateTime DateRented { get; set; }
+
+        [DisplayName("Date Returned")]
+        public DateTime? DateReturned { get; set; }
+
+    }
+}
